444785652df798a13f5ecc938e7c7fc9f233f29b
Patryk Krawiec <patryk.krawiec@student.wat.edu.pl>
2020-12-02 17:03:32 +0100

    Base project example

--- SWP_Biletomat/MainWindow.xaml.cs
+++ SWP_Biletomat/MainWindow.xaml.cs
@@ -12,6 +12,13 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Globalization;
+using Microsoft.Speech;
+using Microsoft.Speech.Recognition;
+using Microsoft.Speech.Synthesis;
+using System.Windows.Threading;
+using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace SWP_Biletomat
 {
@@ -20,10 +27,191 @@ namespace SWP_Biletomat
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        static SpeechRecognitionEngine sre;
+        static SpeechSynthesizer ss;
+        private readonly BackgroundWorker worker = new BackgroundWorker();
+        private int fail_count = 0;
+        private string avaible_oper = "Operacje rozpoznawane \n[+] -> plus, dodaj \n[-] ->minus,odejmij\n[*] -> razy,pomnóż\n[:] -> przez,podziel";
+        private string instruction = "Powiedz \" ile jest \" a następnie działanie w formacie \"liczba\" operacja \"liczba\" ";
         public MainWindow()
         {
             InitializeComponent();
-            //Check
+            Instrucion_lbl.Content = instruction;
+            worker.DoWork += Worker_DoWork;
+            worker.RunWorkerAsync();
+
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            ss = new SpeechSynthesizer();
+            ss.SetOutputToDefaultAudioDevice();
+            ss.Speak("Witam w kalkulatorze");
+            ss.Speak("Powiedz. ile jest. a następnie działanie w formacie liczba operacja liczba ");
+            ss.Speak("Obsługiwane operacje wyświetlają się po prawej stronie okna");
+            CultureInfo ci = new CultureInfo("pl-PL");
+            sre = new SpeechRecognitionEngine(ci);
+            sre.SetInputToDefaultAudioDevice();
+            sre.SpeechRecognized += Sre_SpeechRecognized;
+            Grammar grammar = new Grammar(".\\Grammar\\SimpleGrammar.xml", "rootRule");
+            grammar.Enabled = true;
+            sre.LoadGrammar(grammar);
+            sre.RecognizeAsync(RecognizeMode.Multiple);
+            this.Dispatcher.BeginInvoke(new Action(() => {
+                Failed_rec_lbl.Content = "Niepoprawnie rozpoznanych instrukcji";
+                avaible_operation.Content = avaible_oper;
+                avaible_operation.Visibility = Visibility.Visible;
+                Instrucion_lbl.Visibility = Visibility.Visible;
+                Speech_lbl.Visibility = Visibility.Visible;
+                Recognized_lbl.Visibility = Visibility.Visible;
+            }));
+
+        }
+
+        private void Sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+
+            string txt = e.Result.Text;
+
+            Console.WriteLine(txt);
+            float confidence = e.Result.Confidence;
+            if (confidence >= 0.5)
+            {
+                this.Dispatcher.BeginInvoke(new Action(() => {
+                    Recognized_lbl.Content = "Rozpoznano :" + txt;
+                }));
+                int first = Convert.ToInt32(e.Result.Semantics["first"].Value);
+                int second = Convert.ToInt32(e.Result.Semantics["second"].Value);
+                string operation = e.Result.Semantics["operation"].Value.ToString();
+
+                if (operation == "suma")
+                {
+                    int sum = first + second;
+                    if (sum == 0)
+                    {
+                        ss.Speak("Wynik dodawania wynosi zero");
+
+                    }
+                    else
+                    {
+                        ss.Speak("Wynik dodawania wynosi" + sum.ToString());
+
+                    }
+                    this.Dispatcher.BeginInvoke(new Action(() => {
+                        Speech_lbl.Content = "Wynik dodawania wynosi " + sum.ToString();
+                        History_tb.Text += first + " " + "+ " + second + " = " + sum + "\n";
+                    }));
+
+
+                }
+                else if (operation == "roznica")
+                {
+                    int sum = first - second;
+                    if (sum == 0)
+                    {
+                        ss.Speak("Wynik odejmowania wynosi zero");
+
+                    }
+                    else
+                    {
+                        if (sum < 0)
+                        {
+                            ss.Speak("Wynik odejmowania wynosi minus" + sum.ToString());
+
+                        }
+                        else
+                        {
+                            ss.Speak("Wynik odejmowania wynosi" + sum.ToString());
+
+                        }
+
+                    }
+                    this.Dispatcher.BeginInvoke(new Action(() => {
+                        Speech_lbl.Content = "Wynik odejmowania wynosi " + sum.ToString();
+                        History_tb.Text += first + " " + "- " + second + " = " + sum + "\n";
+
+                    }));
+
+                }
+                else if (operation == "iloczyn")
+                {
+                    int sum = first * second;
+                    if (sum == 0)
+                    {
+                        ss.Speak("Wynik mnożenia wynosi zero");
+
+                    }
+                    else
+                    {
+                        ss.Speak("Wynik mnożenia wynosi" + sum.ToString());
+
+                    }
+                    this.Dispatcher.BeginInvoke(new Action(() => {
+                        Speech_lbl.Content = "Wynik mnożenia wynosi " + sum.ToString();
+                        History_tb.Text += first + " " + "* " + second + " = " + sum + "\n";
+
+                    }));
+                }
+                else if (operation == "iloraz")
+                {
+                    if (second != 0)
+                    {
+                        int sum = first / second;
+                        if (sum == 0)
+                        {
+                            if (first % second == 0)
+                            {
+                                ss.Speak("wynik dzielenia wynosi zero" + " reszty zero");
+
+                            }
+                            else
+                            {
+                                ss.Speak("wynik dzielenia wynosi zero" + " reszty " + first % second);
+
+                            }
+
+                        }
+                        else
+                        {
+                            if (first % second == 0)
+                            {
+                                ss.Speak("wynik dzielenia wynosi " + sum.ToString() + " reszty zero");
+
+                            }
+                            else
+                            {
+                                ss.Speak("wynik dzielenia wynosi " + sum.ToString() + " reszty " + first % second);
+
+                            }
+
+                        }
+                        this.Dispatcher.BeginInvoke(new Action(() => {
+                            Speech_lbl.Content = "Wynik dzielenia wynosi " + sum.ToString() + " reszty " + first % second;
+                            History_tb.Text += first + " " + ": " + second + " = " + sum + " reszty " + first % second + "\n";
+
+                        }));
+                    }
+                    else
+                    {
+                        ss.Speak("nie dziel przez zero");
+                        this.Dispatcher.BeginInvoke(new Action(() => {
+                            Speech_lbl.Content = "Nie dziel przez zero ";
+                        }));
+
+                    }
+                }
+            }
+            else
+            {
+                ss.Speak("Proszę powtórzyć");
+                this.Dispatcher.BeginInvoke(new Action(() => {
+                    Failed_rec_lbl.Visibility = Visibility.Visible;
+                    fail_count++;
+                    failed_count.Content = fail_count;
+                    Recognized_lbl.Content = "Nie rozpoznano, proszę powtórzyć ";
+                }));
+            }
         }
     }
 }
