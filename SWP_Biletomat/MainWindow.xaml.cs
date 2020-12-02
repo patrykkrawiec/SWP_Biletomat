6925d9672d475762d2995a33f3f017cb6d760041
Patryk <krawiec0071@wp.pl>
2020-12-02 17:17:49 +0100

    Change to fit objective

--- SWP_Biletomat/MainWindow.xaml.cs
+++ SWP_Biletomat/MainWindow.xaml.cs
@@ -81,136 +81,13 @@ namespace SWP_Biletomat
                 this.Dispatcher.BeginInvoke(new Action(() => {
                     Recognized_lbl.Content = "Rozpoznano :" + txt;
                 }));
-                int first = Convert.ToInt32(e.Result.Semantics["first"].Value);
-                int second = Convert.ToInt32(e.Result.Semantics["second"].Value);
-                string operation = e.Result.Semantics["operation"].Value.ToString();
-
-                if (operation == "suma")
-                {
-                    int sum = first + second;
-                    if (sum == 0)
-                    {
-                        ss.Speak("Wynik dodawania wynosi zero");
-
-                    }
-                    else
-                    {
-                        ss.Speak("Wynik dodawania wynosi" + sum.ToString());
-
-                    }
-                    this.Dispatcher.BeginInvoke(new Action(() => {
-                        Speech_lbl.Content = "Wynik dodawania wynosi " + sum.ToString();
-                        History_tb.Text += first + " " + "+ " + second + " = " + sum + "\n";
-                    }));
-
-
-                }
-                else if (operation == "roznica")
-                {
-                    int sum = first - second;
-                    if (sum == 0)
-                    {
-                        ss.Speak("Wynik odejmowania wynosi zero");
-
-                    }
-                    else
-                    {
-                        if (sum < 0)
-                        {
-                            ss.Speak("Wynik odejmowania wynosi minus" + sum.ToString());
-
-                        }
-                        else
-                        {
-                            ss.Speak("Wynik odejmowania wynosi" + sum.ToString());
-
-                        }
-
-                    }
-                    this.Dispatcher.BeginInvoke(new Action(() => {
-                        Speech_lbl.Content = "Wynik odejmowania wynosi " + sum.ToString();
-                        History_tb.Text += first + " " + "- " + second + " = " + sum + "\n";
-
-                    }));
-
-                }
-                else if (operation == "iloczyn")
-                {
-                    int sum = first * second;
-                    if (sum == 0)
-                    {
-                        ss.Speak("Wynik mnożenia wynosi zero");
-
-                    }
-                    else
-                    {
-                        ss.Speak("Wynik mnożenia wynosi" + sum.ToString());
-
-                    }
-                    this.Dispatcher.BeginInvoke(new Action(() => {
-                        Speech_lbl.Content = "Wynik mnożenia wynosi " + sum.ToString();
-                        History_tb.Text += first + " " + "* " + second + " = " + sum + "\n";
-
-                    }));
-                }
-                else if (operation == "iloraz")
-                {
-                    if (second != 0)
-                    {
-                        int sum = first / second;
-                        if (sum == 0)
-                        {
-                            if (first % second == 0)
-                            {
-                                ss.Speak("wynik dzielenia wynosi zero" + " reszty zero");
-
-                            }
-                            else
-                            {
-                                ss.Speak("wynik dzielenia wynosi zero" + " reszty " + first % second);
-
-                            }
-
-                        }
-                        else
-                        {
-                            if (first % second == 0)
-                            {
-                                ss.Speak("wynik dzielenia wynosi " + sum.ToString() + " reszty zero");
-
-                            }
-                            else
-                            {
-                                ss.Speak("wynik dzielenia wynosi " + sum.ToString() + " reszty " + first % second);
-
-                            }
-
-                        }
-                        this.Dispatcher.BeginInvoke(new Action(() => {
-                            Speech_lbl.Content = "Wynik dzielenia wynosi " + sum.ToString() + " reszty " + first % second;
-                            History_tb.Text += first + " " + ": " + second + " = " + sum + " reszty " + first % second + "\n";
-
-                        }));
-                    }
-                    else
-                    {
-                        ss.Speak("nie dziel przez zero");
-                        this.Dispatcher.BeginInvoke(new Action(() => {
-                            Speech_lbl.Content = "Nie dziel przez zero ";
-                        }));
-
-                    }
-                }
+               
+                         
             }
             else
             {
                 ss.Speak("Proszę powtórzyć");
-                this.Dispatcher.BeginInvoke(new Action(() => {
-                    Failed_rec_lbl.Visibility = Visibility.Visible;
-                    fail_count++;
-                    failed_count.Content = fail_count;
-                    Recognized_lbl.Content = "Nie rozpoznano, proszę powtórzyć ";
-                }));
+               
             }
         }
     }
