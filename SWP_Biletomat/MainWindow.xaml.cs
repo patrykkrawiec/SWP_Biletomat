3d4c82de2e8dca049c5997569d3b8caba86c5157
Ocaru <kamil.piasecki14@gmail.com>
2021-01-24 09:40:41 +0100

    Main and Ticket update

--- SWP_Biletomat/MainWindow.xaml.cs
+++ SWP_Biletomat/MainWindow.xaml.cs
@@ -21,36 +21,29 @@ namespace SWP_Biletomat
         private int paymentStatus = 0;
         private readonly BackgroundWorker worker = new BackgroundWorker();
         Grammar grammarStart, grammarTickets, grammarFollowingOperation, grammarPayment;
-        private int fail_count = 0;
-        private string avaible_oper = "";
-        private string instruction = "UŻYJ SFORMUŁOWANIA:    \"Poproszę bilet\" + liczba biletów + rodzaj bieletu ";
-        private string example = "PRZYKŁAD:    \"Poproszę bilet, jeden ulgowy dwudziestominutowy\" ";
-        private bool flag1 = false;
+        private string instruction = "UŻYJ SFORMUŁOWANIA:    \"Poproszę bilet\" + (Odpowiedź systemu) + liczba biletów + rodzaj bieletu ";
+        private string example = "PRZYKŁAD:    \"Poproszę bilet, ... jeden ulgowy dwudziestominutowy\" ";
+        private bool initialCondition = true;
         private string recognized; 
         private String ticketString;
         private float ticketPrice=0;
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
+        private void Button_Click(object sender, RoutedEventArgs e){
             paymentStatus = 1;
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
+        private void Button_Click_1(object sender, RoutedEventArgs e){
             paymentStatus = 2;
         }
 
-        public MainWindow()
-        {
+        public MainWindow(){
             InitializeComponent();
             Instrucion_lbl.Content = instruction;
             Exemple_lbl.Content = example;
             worker.DoWork += Worker_DoWork;
             worker.RunWorkerAsync();
-
         }
 
-        private void Worker_DoWork(object sender, DoWorkEventArgs e)
-        {
+        private void Worker_DoWork(object sender, DoWorkEventArgs e){
             ss = new SpeechSynthesizer();
             ss.SetOutputToDefaultAudioDevice();
             ss.Speak("Witam");
@@ -60,37 +53,31 @@ namespace SWP_Biletomat
             sre.SpeechRecognized += Sre_SpeechRecognized;
 
             grammarStart = new Grammar(".\\Grammar\\StartGrammar.xml", "rootRule");
+            grammarTickets = new Grammar(".\\Grammar\\Tickets.xml", "rootRule");
+            grammarFollowingOperation = new Grammar(".\\Grammar\\FollowingOperation.xml", "rootRule");
+            grammarPayment = new Grammar(".\\Grammar\\Payment.xml", "rootRule");
+
             sre.LoadGrammar(grammarStart);
+            sre.LoadGrammar(grammarTickets);
+            sre.LoadGrammar(grammarFollowingOperation);
+            sre.LoadGrammar(grammarPayment); 
             grammarStart.Enabled = true;
 
-            grammarTickets = new Grammar(".\\Grammar\\Tickets.xml", "rootRule");
-            sre.LoadGrammar(grammarTickets);
-
-            grammarFollowingOperation = new Grammar(".\\Grammar\\FollowingOperation.xml", "rootRule");
-            sre.LoadGrammar(grammarFollowingOperation);
-
-            grammarPayment = new Grammar(".\\Grammar\\Payment.xml", "rootRule");
-            sre.LoadGrammar(grammarPayment);
-
             sre.RecognizeAsync(RecognizeMode.Multiple);
-            this.Dispatcher.BeginInvoke(new Action(() =>
-            {
-                Failed_rec_lbl.Content = "Niepoprawnie rozpoznanych instrukcji";
-                avaible_operation.Content = avaible_oper;
-            }));
 
         }
+
         private bool askForAdditions = false;
+
         private void Sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
         {
             recognized = e.Result.Text;
-            StringBuilder sb = new StringBuilder();
 
             Console.WriteLine(recognized);
             float confidence = e.Result.Confidence;
             if (confidence >= 0.7)
             {
-                if ( !flag1 && !e.Result.Semantics["action"].Value.Equals("") && !e.Result.Semantics["ticket"].Value.Equals(""))
+                if ( initialCondition && !e.Result.Semantics["action"].Value.Equals("") && !e.Result.Semantics["ticket"].Value.Equals(""))
                 {
                     grammarTickets.Enabled = true;
                     grammarStart.Enabled = false;
@@ -102,22 +89,13 @@ namespace SWP_Biletomat
                         History_tb.Text += recognized;
                     }));
 
-                    ss.Speak("Proszę podać liczbe i rodzaj biletu");
                     showTicketMachineStatment("Proszę podać liczbe i rodzaj biletu");
-                    flag1 = true;
+                    initialCondition = false;
                 }
                 else
                 {
 
-                    this.Dispatcher.BeginInvoke(new Action(() => {
-                        String temp = History_tb.Text;
-                        History_tb.Text = recognized;
-                        History_tb.Text += "\n" + temp;
-                        Recognized_lbl.Content = "ROZPOZNANO: " + recognized;
-                        Ticket_viewer.Content += ticketString;
-                        ticketString = "";
-                        Price_lbl2.Content = ticketPrice.ToString();
-                    }));
+                    update();
 
                     if (askForAdditions){
                         updateStatus(e);
@@ -130,10 +108,11 @@ namespace SWP_Biletomat
                         payForTickets(e);
                     }
 
+                    update();
+
                 }
             }
             else{
-                ss.Speak("Proszę powtórzyć");
                 showTicketMachineStatment("Proszę powtórzyć");
             }
         }
@@ -143,33 +122,33 @@ namespace SWP_Biletomat
                 askForAdditions = true;
 
                 Ticket ticketFinder = new Ticket();
-                Ticket.TicketType type = ticketFinder.findTicketType(e.Result.Semantics["ticket_type"].Value.ToString() + e.Result.Semantics["ticket_time"].Value.ToString());
+                Ticket.TicketType type = ticketFinder.setTicketType(e.Result.Semantics["ticket_type"].Value.ToString() + e.Result.Semantics["ticket_time"].Value.ToString());
 
                 if (order.Tickets.Exists(x => x.ticketType == type)){
                     int ticketsCount = Convert.ToInt32(e.Result.Semantics["number"].Value);
                     order.Tickets.First(x => x.ticketType == type).count += ticketsCount;
+
+                    ticketString = "\n" + ticketsCount + " x " + type;
+                    ticketPrice += ticketsCount * order.Tickets.First(x => x.ticketType == type).ticketPrice;
+                    Console.WriteLine(ticketPrice.ToString());
                 }
                 else{
                     Ticket ticket = new Ticket();
                     ticket.count = Convert.ToInt32(e.Result.Semantics["number"].Value);
                     ticket.ticketType = type;
                     order.Tickets.Add(ticket);
-                    ticketString = "\n"+ticket.count.ToString() + " x " + ticket.ticketType.ToString();
-                   
-                    ticket.findTicketPrice();
-                    ticketPrice += ticket.count * ticket.ticketPrice;
-                    Console.WriteLine(ticketPrice.ToString());
-                }
 
-                
+                    ticketString = "\n"+ticket.count.ToString() + " x " + ticket.ticketType.ToString();
+                    ticket.setTicketPrice();
+                    ticketPrice += ticket.count * ticket.ticketPrice;
+                    ticket.setTicketTypeName();
+                }
 
                 grammarTickets.Enabled = false;
                 grammarFollowingOperation.Enabled = true;
-                ss.Speak("Czy kontynuować zamówienie?");
                 showTicketMachineStatment("Czy kontynuować zamówienie?");
             }
             else{
-                ss.Speak("Błąd w poleceniu, proszę podać liczbę biletów i rodzaj biletu");
                 showTicketMachineStatment("Błąd w poleceniu, proszę podać liczbę biletów i rodzaj biletu");
             }
 
@@ -182,52 +161,40 @@ namespace SWP_Biletomat
         {
             if (e.Result.Semantics["payment_type"].Value.ToString() == "karta")
             {
-                ss.Speak("Przyłóż kartę do czytnika");
                 showTicketMachineStatment("Przyłóż kartę do czytnika");
-                while (paymentStatus != 1)
-                {
-
-                }
+                while (paymentStatus != 1){}
                 order.isPaid = true;
                 grammarPayment.Enabled = false;
-                ss.Speak("Płatność zakończona, dziękuję. Do widzenia.");
                 showTicketMachineStatment("Płatność zakończona, dziękuję. Do widzenia.");
             }
             if (e.Result.Semantics["payment_type"].Value.ToString() == "gotówka")
             {
-                ss.Speak("Wrzuć monety");
                 showTicketMachineStatment("Wrzuć monety");
-                while (paymentStatus != 2)
-                {
-
-                }
+                while (paymentStatus != 2){}
                 order.isPaid = true;
                 grammarPayment.Enabled = false;
-                ss.Speak("Płatność zakończona, dziękuję. Do widzenia.");
                 showTicketMachineStatment("Płatność zakończona, dziękuję. Do widzenia.");
             }
         }
+
         private void updateStatus(SpeechRecognizedEventArgs e)
         {
             if (e.Result.Semantics["following_operation"].Value.ToString() == "tak")
             {
                 grammarFollowingOperation.Enabled = false;
                 grammarTickets.Enabled = true;
-                ss.Speak("Prosze wskazać bilet");
                 showTicketMachineStatment("Prosze wskazać bilet");
 
             }
             if (e.Result.Semantics["following_operation"].Value.ToString() == "nie")
             {
                 grammarFollowingOperation.Enabled = false;
-                ss.Speak("Wybrane przez Ciebie bilety to ");
                 showTicketMachineStatment("Wybrane przez Ciebie bilety to ");
                 order.isDone = true;
                 foreach (Ticket bilet in order.Tickets)
                 {
-                    ss.Speak(bilet.count + ". " + bilet.ticketType.ToString());
+                    showTicketMachineStatment(bilet.count + ". " + bilet.ticketTypeName);
                 }
-                ss.Speak("Płatność kartą czy gotówką ?");
                 showTicketMachineStatment("Płatność kartą czy gotówką ?");
                 grammarFollowingOperation.Enabled = false;
                 grammarPayment.Enabled = true;
@@ -237,10 +204,30 @@ namespace SWP_Biletomat
 
         private void showTicketMachineStatment(String s)
         {
+            ss.Speak(s);
+
             this.Dispatcher.BeginInvoke(new Action(() => {
                 String temp = History_tb.Text;
                 History_tb.Text = "-"+s;
                 History_tb.Text += "\n" + temp;
+
+            }));
+        }
+
+        private void update()
+        {
+            this.Dispatcher.BeginInvoke(new Action(() => {
+                if (!recognized.Equals(""))
+                {
+                    String temp = History_tb.Text;
+                    History_tb.Text = recognized;
+                    History_tb.Text += "\n" + temp;
+                    Recognized_lbl.Content = "ROZPOZNANO: " + recognized;
+                }
+                Ticket_viewer.Content += ticketString;
+                Price_lbl2.Content = ticketPrice.ToString();
+                ticketString = "";
+                recognized = "";
 
             }));
         }
--- SWP_Biletomat/Ticket.cs
+++ SWP_Biletomat/Ticket.cs
@@ -11,6 +11,9 @@ namespace SWP_Biletomat
         public int count = 0;
         public TicketType ticketType;
         public float ticketPrice;
+        public string ticketTypeName;
+        public string ticketType1;
+        public string ticketType2;
         public enum TicketType
         {
             Normalny20min,
@@ -24,81 +27,94 @@ namespace SWP_Biletomat
         }
 
 
-        public TicketType findTicketType(String s)
+        public TicketType setTicketType(String s)
         {
             switch (s)
             {
                 case "normalnydwudziestominutowy":
                     ticketType = Ticket.TicketType.Normalny20min;
-                    ticketPrice = 3.4f;
                     break;
                 case "normalnysiedemdziesięciopięciominutowy":
                     ticketType = Ticket.TicketType.Normalny75min;
-                    ticketPrice = 4.4f;
                     break;
                 case "normalnydobowy":
                     ticketType = Ticket.TicketType.Normalny24h;
-                    ticketPrice = 15f;
                     break;
                 case "normalnyweekendowy":
                     ticketType = Ticket.TicketType.Normalny72h;
-                    ticketPrice = 36f;
                     break;
                 case "ulgowydwudziestominutowy":
                     ticketType = Ticket.TicketType.Ulgowy20min;
-                    ticketPrice = 1.7f;
                     break;
                 case "ulgowysiedemdziesięciopięciominutowy":
                     ticketType = Ticket.TicketType.Ulgowy75min;
-                    ticketPrice = 2.2f;
                     break;
                 case "ulgowydobowy":
                     ticketType = Ticket.TicketType.Ulgowy24h;
-                    ticketPrice = 7.5f;
                     break;
                 case "ulgowyweekendowy":
                     ticketType = Ticket.TicketType.Ulgowy72h;
-                    ticketPrice = 18f;
                     break;
                 default:
                     break;
             }
-            Console.WriteLine(this.ticketType.ToString());
             return this.ticketType;
         }
 
-        public void findTicketPrice()
+        public void setTicketPrice()
         {
             switch (ticketType)
             {
                 case TicketType.Normalny20min:
                     ticketPrice = 3.4f;
+                    ticketType1 = "normaln";
+                    ticketType2 = "dwudziestominutow";
                     break;
                 case TicketType.Normalny75min:
                     ticketPrice = 4.4f;
+                    ticketType1 = "normaln";
+                    ticketType2 = "siedemdziesięciominutow";
                     break;
                 case TicketType.Normalny24h:
                     ticketPrice = 15f;
+                    ticketType1 = "normaln";
+                    ticketType2 = "dobow";
                     break;
-                    case TicketType.Normalny72h:
+                case TicketType.Normalny72h:
                     ticketPrice = 36f;
+                    ticketType1 = "normaln";
+                    ticketType2 = "weekendow";
                     break;
                 case TicketType.Ulgowy20min:
                     ticketPrice = 1.7f;
+                    ticketType1 = "ulgow";
+                    ticketType2 = "dwudziestominutow";
                     break;
                 case TicketType.Ulgowy75min:
                     ticketPrice = 2.2f;
+                    ticketType1 = "ulgow";
+                    ticketType2 = "siedemdziesięciominutow";
                     break;
                 case TicketType.Ulgowy24h:
                     ticketPrice = 7.5f;
+                    ticketType1 = "ulgow";
+                    ticketType2 = "dobow";
                     break;
                 case TicketType.Ulgowy72h:
                     ticketPrice = 18f;
+                    ticketType1 = "ulgow";
+                    ticketType2 = "weekendow";
                     break;
                 default:
                     break;
             }
-            Console.WriteLine(this.ticketPrice.ToString());
+        }
+
+        public void setTicketTypeName()
+        {
+            if (count == 1) ticketTypeName = ticketType1+"y "+ticketType2+"y ";
+            else if (count >= 2 && count <= 4 ) ticketTypeName = ticketType1 + "e " + ticketType2 + "e ";
+            else if (count >= 5) ticketTypeName = ticketType1 + "ych " + ticketType2 + "ych ";
 
         }
     }
